5e0d90333a71a9f05e6763473b8762b6efc2d4e9
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-05-04 01:58:47 +0900

    Init config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: 'VitePress template',
+	description: 'A JAMstack website template with the VitePress and Netlify CMS config.',
+	themeConfig: {
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Guide', link: '/guide/' },
+			{ text: 'GitHub', link: 'https://github.com/tsutoringo/VitePress-with-Netlify-CMS' }
+		]
+	}
+}
