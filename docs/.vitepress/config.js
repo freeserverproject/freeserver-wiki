b36a6ea9534364d8723efc1af8ee117c8029b81c
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-05-04 12:52:27 +0900

    Add editLink and lastUpdate

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,10 +2,15 @@ module.exports = {
 	title: 'VitePress template',
 	description: 'A JAMstack website template with the VitePress and Netlify CMS config.',
 	themeConfig: {
+		repo: 'tsutoringo/VitePress-with-Netlify-CMS',
+		docsDir: 'docs',
+		editLinks: true,
+		editLinkText: 'Edit this page on GitHub',
+		lastUpdated: 'Last Updated',
 		nav: [
 			{ text: 'Home', link: '/' },
 			{ text: 'Guide', link: '/guide/' },
 			{ text: 'GitHub', link: 'https://github.com/tsutoringo/VitePress-with-Netlify-CMS' }
-		]
+		],
 	}
 }
