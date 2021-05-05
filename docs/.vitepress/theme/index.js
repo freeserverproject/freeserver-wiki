b8dc76ff44f6213aa925b7566c2afc1a45beeb2b
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-05-06 02:47:25 +0900

    コンポーネント登録

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,11 @@
 import DefaultTheme from 'vitepress/theme';
 import McAvatar from './components/McAvatar.vue';
+import DiscordWidget from './components/DiscordWidget/Widget.vue';
 
 export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('mc-avatar', McAvatar)
-  }
+	...DefaultTheme,
+	enhanceApp({ app }) {
+		app.component('mc-avatar', McAvatar);
+		app.component('discord-widget', DiscordWidget);
+	}
 }
