d50849ccec9e8b0ec8b6eb819d01c35b86824072
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-05-11 01:08:23 +0900

    Google Analytics追加

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,10 @@ module.exports = {
 	title: 'FREESERVER Wiki!',
 	description: 'FREESERVER公式のWiki',
 	lang: 'ja-JP',
+	head: [
+		[ 'script', { src: 'https://www.googletagmanager.com/gtag/js?id=G-6SGQWYJL40', async: true } ],
+		[ 'script', {}, `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-6SGQWYJL40');`]
+	],
 	themeConfig: {
 		repo: 'freeserverproject/freeserver-wiki',
 		docsDir: 'docs',
