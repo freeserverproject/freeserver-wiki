96e1bcb3b0150d4c6749c166248fb80993751e7e
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2022-07-22 13:10:14 +0900

    Update Layout.vue

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -9,9 +9,7 @@
 				data-full-width-responsive="true"></ins>
 		</template>
 		<template #page-bottom>
-			<a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.ja">
-				<img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Cc-by-nc-nd_icon.svg" alt="CC BY-NC-ND 4.0">
-			</a>
+			<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="クリエイティブ・コモンズ・ライセンス" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />この 作品 は <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">クリエイティブ・コモンズ 表示 - 非営利 - 改変禁止 4.0 国際 ライセンス</a>の下に提供されています。
 		</template>
 	</Layout>
 </template>
