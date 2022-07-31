48fe9585c3583a35f6da9be1c4e4066a4752a9fa
Nakatai <nakatai0322@gmail.com>
2022-07-31 22:46:12 +0900

    Update Layout.vue

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -9,7 +9,7 @@
 				data-full-width-responsive="true"></ins>
 		</template>
 		<template #page-bottom>
-			<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="CC-by-nc-nd" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />この 記事 は <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">クリエイティブ・コモンズ 表示 - 非営利 - 改変禁止 4.0 国際 ライセンス</a>の下に提供されています。
+			<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="CC-by-nc-nd" style="border-width:0" src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-nc-nd.svg" /></a><br />この記事は <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">クリエイティブ・コモンズ 表示 - 非営利 - 改変禁止 4.0 国際 ライセンス</a>の下に提供されています。
 		</template>
 	</Layout>
 </template>
